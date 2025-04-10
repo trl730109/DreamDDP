10a09e3eaae9ec43b49bb020c1cf9927b9711695
trl730109 <ztangap@connect.ust.hk>
2025-04-10 01:37:31 +0000

    time microbenchmark

--- plots/DearmDDP/process_time.py
+++ plots/DearmDDP/process_time.py
@@ -389,6 +389,7 @@ def cal_pipe_sgd(total_iterations, train_time, comm_time):
 
 def cal_localsgd(total_iterations,nsteps_localsgd, train_time, comm_time):
   time_sum = 0
+
 #   train_time = time_dict[dnn][nworkers]['train']
 #   comm_time = time_dict[dnn][nworkers]['comm']
   for i in range(total_iterations):
@@ -402,14 +403,12 @@ def cal_localsgd(total_iterations,nsteps_localsgd, train_time, comm_time):
 
 def cal_dreamddp(total_iterations, H, waittime_per_H, train_time):
   time_sum = 0
-#   train_time = time_dict[dnn][nworkers]['train']
-#   comm_time = time_dict[dnn][nworkers]['comm']
+
   for i in range(total_iterations):
     if(i != 0 and i % H == 0):
       time_sum += (train_time + waittime_per_H)
     else:
       time_sum += train_time
-
   return time_sum / total_iterations
 
 def cal_pipe_seq_localsgd(total_iterations, H, wait_list, train_time):
@@ -433,40 +432,56 @@ def round_dict_values(input_dict, decimal_places):
 if __name__ == "__main__":
     # servers = ['A6000', 'A800', 'H100']
     # models = ['llama', 'gpt', 'resnet18', 'resnet50']
-    servers = ['A6000']
-    models = ['llama']
-    iterations = 100
+    servers = ['A6000', 'A800', 'H100']
+    models = ['llama', 'gpt', 'resnet18', 'resnet50']
+    iterations = 1000
     nsteps_localsgd = 10
     expected_bandwidth = 10
-    for server in servers:
-        for model in models:
-            current_bandwidth = 10
-            print(f'Server: {server}, Model: {model}, Current Bandwidth: {current_bandwidth}')
-            
-            comm_dict = round_dict_values(scale_comm_time(time_dict[server][model]['comm'], current_bandwidth, expected_bandwidth), 8)
-            bp_dict = round_dict_values(time_dict[server][model]['bp'], 8)
-            
-            total_comm_time = sum(comm_dict.values())
-            total_bp_time = sum(bp_dict.values())
-            print(f'total communication time: {total_comm_time}')
-            print(f'total bp time: {total_bp_time}')
-            
-            plsgd_wait_list, [total_wait_time, schedule, total_iterations], nsteps_localsgd = get_time(comm_dict, bp_dict, nsteps_localsgd)
-            #calculate the extra communication time for partial localsgd and dreamddp
-            partial_localsgd_extra_comm_time = sum(plsgd_wait_list)
-            dreamddp_extra_comm_time = total_wait_time
-            
-            
-            sgd_time_per_iter = cal_sgd(total_iterations, total_bp_time, total_comm_time)
-            pipe_sgd_time_per_iter = cal_pipe_sgd(total_iterations, total_bp_time, total_comm_time)
-            localsgd_time_per_iter = cal_localsgd(total_iterations, nsteps_localsgd, total_bp_time, total_comm_time)
-            dreamddp_time_per_iter = cal_dreamddp(total_iterations, nsteps_localsgd, dreamddp_extra_comm_time, total_bp_time)
-            pipe_seq_localsgd_time_per_iter = cal_pipe_seq_localsgd(total_iterations, nsteps_localsgd, plsgd_wait_list, total_bp_time)
-            
-            print(f'----------------------- Final results -----------------------')
-            print(f'SGD time per iter: {sgd_time_per_iter}')
-            print(f'Pipe SGD time per iter: {pipe_sgd_time_per_iter}')
-            print(f'Localsgd time per iter: {localsgd_time_per_iter}')
-            print(f'Dreamddp time per iter: {dreamddp_time_per_iter}')
-            print(f'Pipe seq Localsgd time per iter: {pipe_seq_localsgd_time_per_iter}')
-            print()
+    
+    # 创建输出文件
+    with open('output.txt', 'w') as f:
+        for server in servers:
+            for model in models:
+                current_bandwidth = 10
+                output_str = f'Server: {server}, Model: {model}, Current Bandwidth: {current_bandwidth}\n'
+                
+                # comm_dict = round_dict_values(scale_comm_time(time_dict[server][model]['comm'], current_bandwidth, expected_bandwidth), 8)
+                # bp_dict = round_dict_values(time_dict[server][model]['bp'], 8)
+                comm_dict = scale_comm_time(time_dict[server][model]['comm'], current_bandwidth, expected_bandwidth)
+                bp_dict = time_dict[server][model]['bp']
+                
+                total_comm_time = sum(comm_dict.values())
+                total_bp_time = sum(bp_dict.values())
+                
+                mean_comm_time = total_comm_time / len(comm_dict)
+                mean_bp_time = total_bp_time / len(bp_dict)
+                output_str += f'total communication time: {total_comm_time}\n'
+                output_str += f'total bp time: {total_bp_time}\n'
+                
+                output_str += f'total layers: {len(comm_dict)}\n'
+                output_str += f'mean layer communication time: {mean_comm_time}\n'
+                output_str += f'mean layer bp time: {mean_bp_time}\n'
+                
+                plsgd_wait_list, [total_wait_time, schedule, total_iterations], nsteps_localsgd = get_time(comm_dict, bp_dict, nsteps_localsgd)
+                #calculate the extra communication time for partial localsgd and dreamddp
+                partial_localsgd_extra_comm_time = sum(plsgd_wait_list)
+                dreamddp_extra_comm_time = total_wait_time
+                
+                sgd_time_per_iter = cal_sgd(iterations, total_bp_time, total_comm_time)
+                pipe_sgd_time_per_iter = cal_pipe_sgd(iterations, total_bp_time, total_comm_time)
+                localsgd_time_per_iter = cal_localsgd(iterations, nsteps_localsgd, total_bp_time, total_comm_time)
+                dreamddp_time_per_iter = cal_dreamddp(iterations, nsteps_localsgd, dreamddp_extra_comm_time, total_bp_time)
+                pipe_seq_localsgd_time_per_iter = cal_pipe_seq_localsgd(total_iterations, nsteps_localsgd, plsgd_wait_list, total_bp_time)
+                
+                output_str += f'----------------------- Final results -----------------------\n'
+                output_str += f'SGD time per iter: {sgd_time_per_iter}\n'
+                output_str += f'Pipe SGD time per iter: {pipe_sgd_time_per_iter}\n'
+                output_str += f'Localsgd time per iter: {localsgd_time_per_iter}\n'
+                output_str += f'Dreamddp time per iter: {dreamddp_time_per_iter}\n'
+                output_str += f'Pipe seq Localsgd time per iter: {pipe_seq_localsgd_time_per_iter}\n\n'
+                
+                # 写入文件
+                f.write(output_str)
+                
+                # 同时打印到控制台
+                print(output_str)
